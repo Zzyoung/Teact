39874b8e36dde69ba333f87d109594865f05d86d
zhangyang <skybabby@126.com>
2018-06-07 22:47:43 +0800

    增加组件

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ interface TeactElement {
 interface instance {
   dom: HTMLElement | Text;
   element: TeactElement;
+  publicInstance?: Component;
   childInstances?: instance[];
+  childInstance?: any;
 }
 
 const TEXTELEMENT = 'TEXT ELEMENT';
@@ -35,15 +37,31 @@ function reconcile(parentDom: HTMLElement, prevInstance: instance | null, elemen
   } else if (element === null) {
     parentDom.removeChild(prevInstance.dom);
     return null;
-  } else if(prevInstance.element.type === element.type){
-    // same type, update props
+  } else if(prevInstance.element.type !== element.type){
+    // replace instance
+    parentDom.replaceChild(newInstance.dom, prevInstance.dom);
+
+    return prevInstance;
+  } else if(typeof element.type === 'string') {
+    // update dom instance
     updateDomProperties(prevInstance.dom, prevInstance.element.props, element.props);
-    prevInstance.element = element;
     prevInstance.childInstances = reconcileChildren(prevInstance, element);
+    prevInstance.element = element;
 
     return prevInstance;
   } else {
-    parentDom.replaceChild(newInstance.dom, prevInstance.dom);
+    // 更新组件
+    prevInstance.publicInstance.props = element.props;
+    const childElement = prevInstance.publicInstance.render(); // 组件的render函数
+    const oldChildInstance = prevInstance.childInstance;
+    const childInstance = reconcile(parentDom, oldChildInstance, childElement)!; // 对比剩下的孩子
+
+    prevInstance.dom = childInstance.dom;
+    prevInstance.childInstance = childInstance;
+    prevInstance.element = element;
+    prevInstance.element = element;
+
+    return prevInstance;
   }
 
   return newInstance;
@@ -69,26 +87,37 @@ function reconcileChildren(prevInstance:instance, element: TeactElement) {
 // 递归instantiate
 function instantiate(element:TeactElement): instance {
   const { type, props } = element;
-  
-  // create DOM element
-  const isTextElement = type === TEXTELEMENT;
-  const dom = isTextElement ? document.createTextNode('') : document.createElement(type);
-  let instance: instance = { dom, element };
+  const isDomElement  = typeof type === 'string';
+  if (isDomElement) {
+    // create DOM element
+    const isTextElement = type === TEXTELEMENT;
+    const dom = isTextElement ? document.createTextNode('') : document.createElement(type);
+    let instance: instance = { dom, element };
 
-  // 更新dom属性，避免创建新dom
-  updateDomProperties(dom, {}, props);
+    // 更新dom属性，避免创建新dom
+    updateDomProperties(dom, {}, props);
 
-  // Render children
-  const childElements = props.children;
-  
-  if (typeof childElements !== 'undefined') {
-    const childInstances = childElements.map(instantiate);
-    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    // Render children
+    const childElements = props.children;
 
-    childDoms.forEach(childDom => dom.appendChild(childDom));
-    instance.childInstances = childInstances;
+    if (typeof childElements !== 'undefined') {
+      const childInstances = childElements.map(instantiate);
+      const childDoms = childInstances.map(childInstance => childInstance.dom);
+
+      childDoms.forEach(childDom => dom.appendChild(childDom));
+      instance.childInstances = childInstances;
+    }
+
+    return instance;
   }
-
+  
+  const instance:any = {};
+  const publicInstance = createPublicInstance(element, instance);
+  const childElement = publicInstance.render();
+  const childInstance = instantiate(childElement);
+  const dom = childInstance.dom;
+  
+  Object.assign(instance, {dom, element, childInstance, publicInstance});
   return instance;
 }
 
@@ -120,18 +149,18 @@ function updateDomProperties(dom:HTMLElement|Text, prevProps: props, nextProps: 
   });
 }
 
-function createTextElement(value:string):TeactElement {
+function createTextElement(value:string|number):TeactElement {
   return createElement(TEXTELEMENT, { nodeValue: value });
 }
 
-function isTeactElement(c: TeactElement | string): c is TeactElement {
-  return typeof (<TeactElement>c).type === 'string';
+function isTeactElement(c: TeactElement | string| number): c is TeactElement {
+  return typeof c !== 'string' && typeof c !== 'number';
 }
 
-function createElement(type: string, config: props, ...args: Array<TeactElement | string>): TeactElement {
+function createElement(type: string, config: props, ...args: Array<TeactElement | string | number>): TeactElement {
   const props = Object.assign({}, config);
   const hasChildren = args.length > 0;
-  const defaultArray: Array<TeactElement|string> = [];
+  const defaultArray: Array<TeactElement|string|number> = [];
   const rawChildren = hasChildren ? defaultArray.concat(...args) : defaultArray;
   props.children = rawChildren.map(c => isTeactElement(c) ? c : createTextElement(c));
 
@@ -141,9 +170,42 @@ function createElement(type: string, config: props, ...args: Array<TeactElement 
   };
 }
 
+
+// Component
+class Component {
+  __internalInstance: any;
+  render:any;
+  constructor(public props: props, public state: Object = {}) {
+
+  }
+  setState(partialState: Object) {
+    this.state = Object.assign({}, this.state, partialState);
+    updateInstance(this.__internalInstance); // 更新虚拟-dom树和更新html
+  }
+}
+
+function createPublicInstance(element: TeactElement, internalInstance: any) {
+  // 当元素进来这里，说明type是一个函数
+  const { type, props } = element;
+  const publicInstance = new type(props);
+
+  publicInstance.__internalInstance = internalInstance; // 内部实例的引用
+  return publicInstance;
+}
+
+function updateInstance(internalInstance:instance) {
+  const parentDom = internalInstance.dom.parentElement;
+  const element = internalInstance.element;
+
+  if (parentDom !== null) {
+    reconcile(parentDom, internalInstance, element);
+  }
+}
+
 window.Teact = {
   render,
-  createElement
+  createElement,
+  Component
 }
 
 // export default {
--- src/test.js
+++ src/test.js
@@ -2,32 +2,138 @@ const randomLikes = () => Math.ceil(Math.random() * 100);
 
 const stories = [
   {
-    name: "Didact introduction",
-    url: "http://bit.ly/2pX7HNn",
+    name: "Teact introduction",
+    url: "http://www.baidu.com",
     likes: randomLikes()
   },
   {
     name: "Rendering DOM elements ",
-    url: "http://bit.ly/2qCOejH",
+    url: "http://www.baidu.com",
     likes: randomLikes()
   },
   {
     name: "Element creation and JSX",
-    url: "http://bit.ly/2qGbw8S",
+    url: "http://www.baidu.com",
     likes: randomLikes()
   },
   {
     name: "Instances and reconciliation",
-    url: "http://bit.ly/2q4A746",
+    url: "http://www.baidu.com",
     likes: randomLikes()
   },
   {
     name: "Components and state",
-    url: "http://bit.ly/2rE16nh",
+    url: "http://www.baidu.com",
     likes: randomLikes()
   }
 ];
 
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var App = function (_Teact$Component) {
+  _inherits(App, _Teact$Component);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  }
+
+  _createClass(App, [{
+    key: "render",
+    value: function render() {
+      return Teact.createElement(
+        "div",
+        null,
+        Teact.createElement(
+          "h1",
+          null,
+          "Teact Stories"
+        ),
+        Teact.createElement(
+          "ul",
+          null,
+          this.props.stories.map(function (story) {
+            return Teact.createElement(Story, { name: story.name, url: story.url });
+          })
+        )
+      );
+    }
+  }]);
+
+  return App;
+}(Teact.Component);
+
+var Story = function (_Teact$Component2) {
+  _inherits(Story, _Teact$Component2);
+
+  function Story(props) {
+    _classCallCheck(this, Story);
+
+    var _this2 = _possibleConstructorReturn(this, (Story.__proto__ || Object.getPrototypeOf(Story)).call(this, props));
+
+    _this2.state = { likes: Math.ceil(Math.random() * 100) };
+    return _this2;
+  }
+
+  _createClass(Story, [{
+    key: "like",
+    value: function like() {
+      this.setState({
+        likes: this.state.likes + 1
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      var _props = this.props,
+        name = _props.name,
+        url = _props.url;
+      var likes = this.state.likes;
+
+      var likesElement = Teact.createElement("span", null);
+      return Teact.createElement(
+        "li",
+        null,
+        Teact.createElement(
+          "button",
+          {
+            onClick: function onClick(e) {
+              return _this3.like();
+            }
+          },
+          likes,
+          Teact.createElement(
+            "b",
+            null,
+            "\u2764\uFE0F"
+          )
+        ),
+        Teact.createElement(
+          "a",
+          { href: url },
+          name
+        )
+      );
+    }
+  }]);
+
+  return Story;
+}(Teact.Component);
+
+Teact.render(Teact.createElement(App, { stories: stories }), document.getElementById("root"));
+
+/*
 var appElement = function appElement() {
   return Teact.createElement(
     "div",
@@ -72,3 +178,4 @@ function handleClick(story) {
 }
 
 Teact.render(appElement(), document.getElementById("root"));
+*/
